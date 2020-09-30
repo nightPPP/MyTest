0868b6a187dd6d607cd25619292e41f8b80f349a
liuyang <ssi-lyang@dfmc.com.cn>
2020-09-30 10:23:07 +0800

    自定义控件1--画笔

---
+++ app/src/main/java/com/example/mytest/myselfView/PaintTest/BasisView.java
@@ -0,0 +1,42 @@
+package com.example.mytest.myselfView.PaintTest;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class BasisView extends View {
+    public BasisView(Context context) {
+        super(context);
+    }
+
+    public BasisView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BasisView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        paint.setStyle(Paint.Style.FILL);
+        paint.setStrokeWidth(50);
+        //圆心的横向x坐标190，纵向y坐标200， 圆的半径150
+        canvas.drawCircle(190,200,150,paint);
+
+        paint.setColor(Color.BLUE);
+        canvas.drawCircle(190, 200, 100, paint);
+
+        paint.setColor(Color.YELLOW);
+        canvas.drawCircle(190, 200, 50, paint);
+    }
+}
--- app/src/main/java/com/example/mytest/myselfView/TestMyselfViewActivity.java
+++ app/src/main/java/com/example/mytest/myselfView/TestMyselfViewActivity.java
@@ -17,14 +17,14 @@ public class TestMyselfViewActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_self_view);
-        MenuItemLayout view = (MenuItemLayout) findViewById(R.id.menu_1);
-        view.setTitleText("666");
-        view.setViewOnlickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        MenuItemLayout view = (MenuItemLayout) findViewById(R.id.menu_1);
+//        view.setTitleText("666");
+//        view.setViewOnlickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
 
 
     }
